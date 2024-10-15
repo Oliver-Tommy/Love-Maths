8b5b2492a42f40ad0ad30af9332ae0a22a00efa3
Thomas Oliver <oliver.tommy@icloud.com>
2024-10-15 13:56:52 +0000

    Add calculator for the correct answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
 //  Wait for the DOM to finish loading before runnig the game
 // Get the button elements and add event listeners
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByTagName("button");
 
-    for (let button of buttons){
-        button.addEventListener("click", function() {
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked Submit!");
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
@@ -38,11 +38,28 @@ function runGame(gameType) {
 
 }
 
+/**
+ * Checks if the users answer is correct or wrong
+ */
 function checkAnswer() {
-
+    
 }
 
+/**
+ * Gets the oprands (numbers) and the operator directly from the DOM, and returns the correct answer
+ */
 function calculateCorrectAnswer() {
+    // Getting values back from the DOM 
+    let operand1 = parseInt(document.getElementById("operand1").innerText);
+    let operand2 = parseInt(document.getElementById("operand2").innerText);
+    let operator = document.getElementById("operator").innerText;
+
+    if (operator === "+") {
+        return [operand1 + operand2, "addition"];
+    } else {
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}. Aborting`;
+    }
 
 }
 
